dc8a34c5fb2f6f0f2db43d92bf7bf4d1c5efc428
Caleybob <drxpl0de18@gmail.com>
2025-10-25 22:28:59 +0100

    Finished Project

---
+++ README.md
@@ -0,0 +1,17 @@
+### README.md Generator
+
+## Description
+
+This project generates README.md files with user input through InquirerPy (this README was actually made with this project).
+
+## Usage Information
+
+Leave a blank input on any section you don't want to use.
+
+# License Information
+
+This project is not licensed. All rights reserved.
+
+# Author
+
+By Caolan Boyce
--- main.py
+++ main.py
@@ -1,47 +1,100 @@
-from InquirerPy import prompt, inquirer
+from InquirerPy import prompt
 from pathlib import Path
+import sys
 
-script_path = Path(__file__)
 
-output_file_directory = script_path.resolve().parent
+class ReadmeGenerator:
+    def __init__(self):
+        self.questions = [
+            {"type": "input", "message": "Project Title:", "name": "project_title"},
+            {"type": "input", "message": "description", "name": "description"},
+            {"type": "input", "message": "Installation Instructions:", "name": "install_info"},
+            {"type": "input", "message": "Usage Information:", "name": "usage_info"},
+            {
+                "type": "list",
+                "message": "Which License would you like to choose?:",
+                "name": "license",
+                "choices": [
+                    "Unlicensed / All Rights Reserved",
+                    "MIT License",
+                    "Apache License 2.0",
+                    "GPL v3",
+                    "BSD 3-Clause License",
+                ],
+            },
+            {"type": "input", "message": "Author Name:", "name": "author_name"},
+            {"type": "input", "message": "Contact Information:", "name": "contact_info"},
+            {"type": "confirm", "message": "Confirm?", "name": "confirm"},
+        ]
 
-output_file = output_file_directory / "README.md"
+        self.license_texts = {
+            "MIT License": "This project is licensed under the MIT License. See the LICENSE file for details.",
+            "Apache License 2.0": "This project is licensed under the Apache License 2.0. See the LICENSE file for details.",
+            "GPL v3": "This project is licensed under the GNU General Public License v3.0 (GPL-3.0). See the LICENSE file for details.",
+            "BSD 3-Clause License": "This project is licensed under the BSD 3-Clause License. See the LICENSE file for details.",
+            "Unlicensed / All Rights Reserved": "This project is not licensed. All rights reserved.",
+        }
 
-with open(output_file, 'w') as file:
-    file.write('some text')
+    def run(self):
+        result = prompt(self.questions)
 
-questions  = [
+        if not result["confirm"]:
+            sys.exit()
 
-    {"type": "input", "message": "Project Title:", "name": "project_title"},
+        script_path = Path(__file__)
+        output_file_directory = script_path.resolve().parent
+        output_file = output_file_directory / "README.md"
 
-    {"type": "input", "message": "description", "name": "description"},
+        with open(output_file, "w", encoding="utf-8") as file:
+            print("\n\n") 
 
-    {"type": "input", "message": "Installation Instructions:", "name": "install_info"},
+        if result["project_title"]:
+            with open(output_file, "a", encoding="utf-8") as file:
+                file.write(f'### {result["project_title"]}')
 
-    {"type": "input", "message": "Usage Information", "name": "usage_info"},
+        if result["description"]:
+            with open(output_file, "a", encoding="utf-8") as file:
+                file.write("\n\n")
+                file.write("## Description\n\n")
+                file.write(f'{result["description"]}')
+       
+        if result["install_info"]:
+            with open(output_file, "a", encoding="utf-8") as file:
+                file.write("\n\n")
+                file.write("## Installation Instructions\n\n")
+                file.write(f'{result["install_info"]}')
     
-    {
-        "type": "list",
-        "message": "Which License would you like to choose?:", "name": "license",
-        "choices": ["MIT License", "Apache License 2.0", "GPL v3", "BSD 3-Clause License", "Unlicensed / All Rights Reserved"],
-    },
+        if result["usage_info"]:
+            with open(output_file, "a", encoding="utf-8") as file:
+                file.write("\n\n")
+                file.write("## Usage Information\n\n")
+                file.write(f'{result["usage_info"]}')
 
-    {"type": "input", "message": "Author Name:", "name": "author_name"},
+        with open(output_file, "a", encoding="utf-8") as file:
+            file.write("\n\n")
+            file.write("# License Information\n\n")
+            file.write(self.license_texts.get(result["license"]))
+   
+        if result["author_name"]:
+            with open(output_file, "a", encoding="utf-8") as file:
+                file.write("\n\n")
+                file.write("# Author\n\n")
+                file.write(f'By {result["author_name"]}')
 
-    {"type": "input", "message": "Contact Information:", "name": "contact_info"},
+        if result["contact_info"]:
+            with open(output_file, "a", encoding="utf-8") as file:
+                file.write("\n\n")
+                file.write("# Contact Information\n\n")
+                file.write(f'By {result["contact_info"]}')
 
-     {"type": "confirm", "message": "Confirm?"},
-]  
+
+if __name__ == "__main__":
+    generator = ReadmeGenerator()
+    generator.run()
 
 
-result = prompt(questions)
-print(f'Project Title: {result["project_title"]}')
-print(f'Description: {result["description"]}')
-print(f'Installation Instructions: {result["install_info"]}')
-print(f'Usage Information {result["usage_info"]}')
-print(f'License: {result["license"]}')
-print(f'Author: {result["author_name"]}')
-print(f'Contact information: {result["contact_info"]}')
+
+        
 
 
 
